c907d87cec66abaefd553dae44d041041df70156
E-Hussain06 <ehsanhussain991@gmail.com>
2017-04-08 16:06:48 +0100

    subscribing to observables/reactive ext, setting up http request, exception handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ 
-    AppComponent, 
-    ProductListComponent, 
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
     ProductFilterPipe,
-    StarComponent ],
-  bootstrap: [ AppComponent ]
+    StarComponent
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
