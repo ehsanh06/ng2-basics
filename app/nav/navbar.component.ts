0496e9803de8cbdc785305568bbd9b2c585f580a
E-Hussain06 <ehsanhussain991@gmail.com>
2017-05-15 14:08:27 +0100

    implemented search session logic

--- app/nav/navbar.component.ts
+++ app/nav/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../user/auth.service'
+import { ISession, EventService } from "../events/index";
+
 @Component({
     selector: 'nav-bar',
     templateUrl: 'app/nav/navbar.component.html',
@@ -10,9 +12,23 @@ import { AuthService } from '../user/auth.service'
             #searchFrom {display:none}
         }
         li > a.active { color: #F97924; }
-    `]    
+    `]
 })
 
-export class NavBarComponent { 
-    constructor(private auth: AuthService) {}
- }
+export class NavBarComponent {
+    searchTerm: string = "";
+    foundSessions: ISession[]
+
+    constructor(private auth: AuthService, private eventService: EventService) {
+
+     }
+
+    searchSessions(searchTerm) {
+        this.eventService.searchSessions(searchTerm).subscribe(
+            sessions => {
+                this.foundSessions = sessions;
+                console.log(this.foundSessions);
+            }
+        )
+    }
+}
